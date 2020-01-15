9db75dbb9d8f14c7d4e2a82c0dc5ee9f1cd01ba4
wangcc7 <2375722956@qq.com>
2020-01-15 16:04:52 +0800

    2020/01/15

--- geekbang/Small_lesson_7 day/day_1_20200115.py
+++ geekbang/Small_lesson_7 day/day_1_20200115.py
@@ -3,6 +3,16 @@
 # mail:wangcc_sd@163.com
 
 #算法第一天
+class Solution:
+    def removeDuplicates(self, nums):
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+        return j+1
 
 if __name__ == '__main__':
-    pass
+    S1 = Solution()
+    a = S1.removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+    print(a)
